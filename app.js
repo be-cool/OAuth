1fa824ef34d4af89aa97c0b41fd0c7b7ad716ace
Ben Cooley <bcooley58@gmail.com>
2019-09-03 16:21:44 -0400

    working through the 9th oauth video clear

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const authRoutes = require('./views/routes/auth-routes');
+const passportSetup = require('./views/config/passport-setup');
+
 const app = express();
 
 // set a view engine
 app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.render('home')
---
+++ views/config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+google: {
+    clientID: "514952547601-a986t90dtpaas7cgfnlvga27ojpe05n7.apps.googleusercontent.com",
+    clientSecret: "gGi0CebaeOGSbtRWS0heLy-n"
+}
+};
